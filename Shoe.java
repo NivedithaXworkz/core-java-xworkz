5161f99750970e65399274740302aa80dd72b9f2
unknown <nivedithajsintre22.workz@gmail.com>
2023-08-21 20:50:38 +0530

    See the Shoe

---
+++ Shoe.java
@@ -0,0 +1,14 @@
+class Shoe{
+public static void main(String walkingshoe[]){
+	String sole = "Rubber";
+	String colour = "Pink";
+	String closureType = "Lace-up";
+	String name = "Puma";
+	String countryofOrigin = "India";
+System.out.println(" The Sole is" + sole);
+System.out.println("The Colour is " +colour );
+System.out.println("The Closure Type is " + closureType);
+System.out.println("The Name is " + name);
+System.out.println("The Country of Origin is " + countryofOrigin );
+}
+}
